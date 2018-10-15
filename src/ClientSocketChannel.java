3c3d434df2a8b01697eb9b475573be60cbab0f22
Oscarchoi <wychoi502@gmail.com>
2018-10-15 10:32:53 +0900

    choi

--- src/ClientSocketChannel.java
+++ src/ClientSocketChannel.java
@@ -1,19 +1,27 @@
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
 import java.nio.charset.Charset;
 
 public abstract class ClientSocketChannel extends SocketChannel {
 
-    ClientSocketChannel(){
-        super.;
+    ClientSocketChannel(SelectorProvider provider){
+        super(provider);
     };
 
     public String receive(){
-        Charset charset = Charset.forName("UTF-8");
-        ByteBuffer buffer = null;
-        buffer = ByteBuffer.allocate(100);
-        int bytecount = super.read(buffer);
-        return charset.decode(buffer).toString();
+        try {
+            Charset charset = Charset.forName("UTF-8");
+            ByteBuffer buffer = null;
+            buffer = ByteBuffer.allocate(100);
+            int bytecount = read(buffer);
+            buffer.flip();
+            return charset.decode(buffer).toString();
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
