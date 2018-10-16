23885855d0b2ee22f6c6b945b9347cb434e575bc
Oscarchoi <wychoi502@gmail.com>
2018-10-16 14:47:23 +0900

    Port Edit

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
-
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
@@ -10,50 +11,51 @@ import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class Client {
-
+    public static Charset charset = Charset.forName("UTF-8");
     SocketChannel sock;
-    String host;
-    int port;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         if (args.length <3) {
             System.out.printf("Usage: %s [SERVER_ADDRESS] [TCP_PORT] [My_ID]\n",args[0]);
             exit(1);
         }
 
-        Scanner scan = new Scanner(System.in);
+        InetAddress host = Inet4Address.getByName(args[0]);
+        int port = Integer.valueOf(args[1]);
         String ID = args[2];
 
+        Scanner scan = new Scanner(System.in);
+
         Client client = new Client();
+        ByteBuffer byteBuffer1;
+        byteBuffer1 = ByteBuffer.allocate(1024);
 
         while (true) {
             try {
-                client.connect();
+                System.out.println("host: " + host);
+                System.out.println("port: "+ port);
+                client.connect(host,port);
                 ByteBuffer byteBuffer = null;
-                Charset charset = Charset.forName("UTF-8");
+
                 byteBuffer = charset.encode("/id " + ID);
                 client.sock.write(byteBuffer);
 
-                ByteBuffer byteBuffer1;
-                byteBuffer1 = ByteBuffer.allocate(1024);
+
                 client.sock.read(byteBuffer1);
                 byteBuffer1.flip();
-
                 String ret = charset.decode(byteBuffer1).toString();
                 System.out.println(ret);
 
-                if (ret.substring(0, 3).compareTo("yes") == 0) {
+                if (ret.length()>=3 && (ret.substring(0, 3).compareTo("yes") == 0)) {
                     System.out.println("ID check Success.");
                     break;
-                } else {
-                    System.out.println("이미 존재하는 ID 입니다. 다시 시도하십시오... ");
-                    ID = scan.nextLine();
                 }
-
+                System.out.println("이미 존재하는 ID 입니다. 다시 시도하십시오... ");
+                ID = scan.nextLine();
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                System.out.println("connect exception");
+                return;
             }
         }
             client.read();
@@ -61,20 +63,14 @@ public class Client {
         }
 
 
-
-    public void connect() throws UnknownHostException, IOException {
-        host = "localhost";
-        port = 7001;
+    public void connect(InetAddress host, int port) throws UnknownHostException, IOException {
         sock = SocketChannel.open();
         sock.connect(new InetSocketAddress(host, port));
     }
 
     public void read() {
-
         Thread thread = new Thread() {
-
             @Override
-
             public void run() {
 
                 // TODO Auto-generated method stub
@@ -83,32 +79,22 @@ public class Client {
                     try {
                         ByteBuffer byteBuffer = ByteBuffer.allocate(100);
                         int byteCount = sock.read(byteBuffer);
-                        Charset charset = Charset.forName("UTF-8");
                         String message = charset.decode(byteBuffer).toString();
                         System.out.println("[데이터 받기 성공]: " + message);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-
-                // TODO Auto-generated catch block
-
-
             }
 
         };
         thread.start();
-
     }
 
     public void chatStart() {
-
         Scanner scan = new Scanner(System.in);
-
         while (true) {
-
             String message = scan.nextLine();
-
             System.out.println(message);
             try {
                 ByteBuffer byteBuffer = null;
@@ -117,31 +103,18 @@ public class Client {
                 sock.write(byteBuffer);
 
             } catch (IOException e) {
-
                 // TODO Auto-generated catch block
-
                 System.out.println("client write exception!");
-
                 try {
-
                     sock.close();
                     break;
-
                 } catch (IOException e1) {
-
                     // TODO Auto-generated catch block
-
                     e1.printStackTrace();
-
                 }
-
                 e.printStackTrace();
-
             }
-
         }
-
     }
-
 }
 
